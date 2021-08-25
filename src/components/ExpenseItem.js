2b21bb60c2bdbde1a68f524d2ae2c091850d1a89
levvw <abdelraahmanmostafa@gmail.com>
2021-08-25 14:44:31 +0200

    simplified files by creating nested props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,9 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate.js";
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-us", { month: "long" });
-  const day = props.date.toLocaleString("en-us", { day: "2-digit" });
-  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
